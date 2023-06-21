94ac7040e179c25422b28cfca1240107f82f1461
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-06-21 16:35:38 -0700

    Delete duplicate folder

--- src-no-context/components/Footer.tsx
+++
@@ -1,4 +0,0 @@
-function Footer({ children }: { children: React.ReactNode }) {
-  return <footer>{children}</footer>;
-}
-export default Footer;
--- src-no-context/components/Main.tsx
+++
@@ -1,4 +0,0 @@
-function Main({ children }: { children?: React.ReactNode }) {
-  return <main className="main">{children}</main>;
-}
-export default Main;
